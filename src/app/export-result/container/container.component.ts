bd361c7c0e65d65ccc1484cefa8c0898f94f1156
GOPU-KARTHIK-07 <2000030281cse@gmail.com>
2025-10-31 10:16:59 +0530

    setup

---
+++ src/app/export-result/container/container.component.ts
@@ -0,0 +1,8 @@
+import { ChangeDetectionStrategy, Component, Input } from "@angular/core";
+@Component({
+  selector: "app-container",
+  templateUrl: "./container.component.html",
+  styleUrls: ["./container.component.scss"],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ContainerComponent {}
